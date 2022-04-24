ffae494c089dc470c241f58d97f840f7137952f6
akinankarali <akbeta08@gmail.com>
2022-04-24 14:20:15 +0300

    feat: add api requests to home page

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+    export async function load({ fetch }) {
+        const resMatch = await fetch('../matches.json');
+        const resPlayer = await fetch('../players.json');
+        const matchData = await resMatch.json();
+        const playerData = await resPlayer.json();
+
+        if(resMatch.ok && resPlayer.ok) {
+            return {
+                props: {
+                   matches: matchData,
+                   players: playerData,
+                }
+            }
+        }
+    }
+</script>
+
+<script>
+    import Matches from '../components/Matches.svelte';
+    export let matches;
+    export let players;
+</script>
+
+<section>
+    <Matches {matches} {players}/>
+</section>
