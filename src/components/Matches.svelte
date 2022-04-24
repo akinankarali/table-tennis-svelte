ece1d18e47b739c4e2ed6d488cad507e469b5e97
akinankarali <akbeta08@gmail.com>
2022-04-24 14:21:12 +0300

    feat: create matches component

---
+++ src/components/Matches.svelte
@@ -0,0 +1,24 @@
+<script>
+    import MatchCard from "./MatchCard.svelte";
+    export let matches
+    export let players
+</script>
+
+<h3>Matches</h3>
+<div class="matches">
+    {#each matches as match}
+        <MatchCard {match} {players}/>
+    {/each}
+</div>
+
+<style>
+    h3 {
+        padding: 0 1rem;
+    }
+    .matches {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        grid-column-gap: 1rem;
+        grid-row-gap: 2rem;
+    }
+</style>
